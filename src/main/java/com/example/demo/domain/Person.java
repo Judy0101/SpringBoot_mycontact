90b656a4a734437de2f96fc790536e42b3726bcd
Haeji <haillydev@gmail.com>
2021-04-24 08:39:44 +0900

    Add Constructor anotaion

--- src/main/java/com/example/demo/domain/Person.java
+++ src/main/java/com/example/demo/domain/Person.java
@@ -1,50 +1,45 @@
 package com.example.demo.domain;
 
+import com.sun.istack.NotNull;
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor // 인자 없는 생성자
+@AllArgsConstructor //모든 인자를 가진 생성자
+@RequiredArgsConstructor // 꼭 필요한 인자 정해주기 (not null anotation으로)
 public class Person {
 
     @Id
     @GeneratedValue
     private Long id;
 
+    @NotNull
     private String name;
 
+    @NotNull
     private int age;
 
-    public Long getId() {
-        return id;
-    }
+    private String hobby;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private String bloodType;
 
-    public String getName() {
-        return name;
-    }
+    private String address;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    private LocalDate birthday;
 
-    public int getAge() {
-        return age;
-    }
+    private String job;
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+    @ToString.Exclude
+    private String phoneNumber;
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
+
+
 }
--- src/test/java/com/example/demo/repository/PersonRepositoryTest.java
+++ src/test/java/com/example/demo/repository/PersonRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,5 +31,10 @@ class PersonRepositoryTest {
         assertThat(people.size()).isEqualTo(1);
         assertThat(people.get(0).getName()).isEqualTo("martin");
         assertThat(people.get(0).getAge()).isEqualTo(10);
+    }
+
+    @Test
+    void allArgsConstructor(){
+        Person person = new Person(1L, "martin", 10 , "reading", "A", "분당", LocalDate.of(2019, 1, 1), "progreammer", "010");
     }
 }
