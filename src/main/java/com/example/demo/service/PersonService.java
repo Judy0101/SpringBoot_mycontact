288e4308849c4f2931e4401c7b81c821e10f5645
Haeji <haillydev@gmail.com>
2021-04-24 20:48:41 +0900

    JPA Relation One to One

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -4,13 +4,16 @@ import com.example.demo.domain.Block;
 import com.example.demo.domain.Person;
 import com.example.demo.repository.BlockRepository;
 import com.example.demo.repository.PersonRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class PersonService {
 
     @Autowired
@@ -25,5 +28,15 @@ public class PersonService {
   //      List<String> blockNames = blocks.stream().map(Block::getName).collect(Collectors.toList());
 
         return people.stream().filter(person -> person.getBlock() != null).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public Person getPerson(Long id){
+        Person person = personRepository.findById(id).get();
+
+        System.out.println("person : " + person);
+        log.info("person : {}", person);
+
+        return person;
     }
 }
